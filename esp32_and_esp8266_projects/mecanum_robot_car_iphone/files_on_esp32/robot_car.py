fe1042e087e754a2ef27648a1f428787a997ba1c
Frank Siyli <frank.siyli@gmail.com>
2024-04-01 21:50:54 +0200

    iphone live server changes

--- esp32_and_esp8266_projects/mecanum_robot_car_iphone/files_on_esp32/robot_car.py
+++ esp32_and_esp8266_projects/mecanum_robot_car_iphone/files_on_esp32/robot_car.py
@@ -2,28 +2,39 @@ from machine import Pin, PWM
 
 """
 Class to represent our robot car
+
+
 """
-class RobotCar():
-    def __init__(self, front_enable_pins, rear_enable_pins, front_motor_pins, rear_motor_pins, speed):
+
+
+class RobotCar:
+    def __init__(
+        self,
+        front_enable_pins,
+        rear_enable_pins,
+        front_motor_pins,
+        rear_motor_pins,
+        speed,
+    ):
         self.front_right_motor_enable_pin = PWM(Pin(front_enable_pins[0]), freq=2000)
         self.front_left_motor_enable_pin = PWM(Pin(front_enable_pins[1]), freq=2000)
         self.rear_right_motor_enable_pin = PWM(Pin(rear_enable_pins[0]), freq=2000)
         self.rear_left_motor_enable_pin = PWM(Pin(rear_enable_pins[1]), freq=2000)
-        
+
         self.front_right_motor_control_1 = Pin(front_motor_pins[0], Pin.OUT)
         self.front_right_motor_control_2 = Pin(front_motor_pins[1], Pin.OUT)
         self.front_left_motor_control_1 = Pin(front_motor_pins[2], Pin.OUT)
         self.front_left_motor_control_2 = Pin(front_motor_pins[3], Pin.OUT)
-        
+
         self.rear_right_motor_control_1 = Pin(rear_motor_pins[0], Pin.OUT)
         self.rear_right_motor_control_2 = Pin(rear_motor_pins[1], Pin.OUT)
         self.rear_left_motor_control_1 = Pin(rear_motor_pins[2], Pin.OUT)
         self.rear_left_motor_control_2 = Pin(rear_motor_pins[3], Pin.OUT)
-        
+
         self.speed = speed
-        
+
     def stop(self):
-        print('Car stopping')
+        print("Car stopping")
         self.front_right_motor_control_1.value(0)
         self.front_right_motor_control_2.value(0)
         self.front_left_motor_control_1.value(0)
@@ -32,19 +43,19 @@ class RobotCar():
         self.rear_right_motor_control_2.value(0)
         self.rear_left_motor_control_1.value(0)
         self.rear_left_motor_control_2.value(0)
-        
+
         self.front_right_motor_enable_pin.duty_u16(0)
         self.front_left_motor_enable_pin.duty_u16(0)
         self.rear_right_motor_enable_pin.duty_u16(0)
         self.rear_left_motor_enable_pin.duty_u16(0)
-        
+
     def forward(self):
-        print('Move forward')
+        print("Move forward")
         self.front_right_motor_enable_pin.duty_u16(self.speed)
         self.front_left_motor_enable_pin.duty_u16(self.speed)
         self.rear_right_motor_enable_pin.duty_u16(self.speed)
         self.rear_left_motor_enable_pin.duty_u16(self.speed)
-        
+
         self.front_right_motor_control_1.value(1)
         self.front_right_motor_control_2.value(0)
         self.front_left_motor_control_1.value(1)
@@ -54,14 +65,13 @@ class RobotCar():
         self.rear_left_motor_control_1.value(1)
         self.rear_left_motor_control_2.value(0)
 
-    
     def reverse(self):
-        print('Move reverse')
+        print("Move reverse")
         self.front_right_motor_enable_pin.duty_u16(self.speed)
         self.front_left_motor_enable_pin.duty_u16(self.speed)
         self.rear_right_motor_enable_pin.duty_u16(self.speed)
         self.rear_left_motor_enable_pin.duty_u16(self.speed)
-        
+
         self.front_right_motor_control_1.value(0)
         self.front_right_motor_control_2.value(1)
         self.front_left_motor_control_1.value(0)
@@ -70,44 +80,76 @@ class RobotCar():
         self.rear_right_motor_control_2.value(1)
         self.rear_left_motor_control_1.value(0)
         self.rear_left_motor_control_2.value(1)
-    
-    def turnLeft(self):
-        print('Turning Left')
+
+    def left(self):
+        print("Turning Left")
         self.front_right_motor_enable_pin.duty_u16(self.speed)
         self.front_left_motor_enable_pin.duty_u16(self.speed)
         self.rear_right_motor_enable_pin.duty_u16(self.speed)
         self.rear_left_motor_enable_pin.duty_u16(self.speed)
-        
-        self.front_right_motor_control_1.value(1)
-        self.front_right_motor_control_2.value(0)
-        self.front_left_motor_control_1.value(0)
+
+        self.front_right_motor_control_1.value(0)
+        self.front_right_motor_control_2.value(1)
+        self.front_left_motor_control_1.value(1)
         self.front_left_motor_control_2.value(0)
         self.rear_right_motor_control_1.value(1)
         self.rear_right_motor_control_2.value(0)
         self.rear_left_motor_control_1.value(0)
-        self.rear_left_motor_control_2.value(0)
-    
-    def turnRight(self):
-        print('Turning Right')
+        self.rear_left_motor_control_2.value(1)
+
+    def right(self):
+        print("Turning Right")
         self.front_right_motor_enable_pin.duty_u16(self.speed)
         self.front_left_motor_enable_pin.duty_u16(self.speed)
         self.rear_right_motor_enable_pin.duty_u16(self.speed)
         self.rear_left_motor_enable_pin.duty_u16(self.speed)
-        
-        self.front_right_motor_control_1.value(0)
+
+        self.front_right_motor_control_1.value(1)
         self.front_right_motor_control_2.value(0)
+        self.front_left_motor_control_1.value(0)
+        self.front_left_motor_control_2.value(1)
+        self.rear_right_motor_control_1.value(0)
+        self.rear_right_motor_control_2.value(1)
+        self.rear_left_motor_control_1.value(1)
+        self.rear_left_motor_control_2.value(0)
+
+    def spinLeft(self):
+        print("Spinning Left")
+        self.front_right_motor_enable_pin.duty_u16(self.speed)
+        self.front_left_motor_enable_pin.duty_u16(self.speed)
+        self.rear_right_motor_enable_pin.duty_u16(self.speed)
+        self.rear_left_motor_enable_pin.duty_u16(self.speed)
+
+        self.front_right_motor_control_1.value(1)
+        self.front_right_motor_control_2.value(0)
+        self.front_left_motor_control_1.value(0)
+        self.front_left_motor_control_2.value(1)
+        self.rear_right_motor_control_1.value(1)
+        self.rear_right_motor_control_2.value(0)
+        self.rear_left_motor_control_1.value(0)
+        self.rear_left_motor_control_2.value(1)
+
+    def spinRight(self):
+        print("Spinning Right")
+        self.front_right_motor_enable_pin.duty_u16(self.speed)
+        self.front_left_motor_enable_pin.duty_u16(self.speed)
+        self.rear_right_motor_enable_pin.duty_u16(self.speed)
+        self.rear_left_motor_enable_pin.duty_u16(self.speed)
+
+        self.front_right_motor_control_1.value(0)
+        self.front_right_motor_control_2.value(1)
         self.front_left_motor_control_1.value(1)
         self.front_left_motor_control_2.value(0)
         self.rear_right_motor_control_1.value(0)
-        self.rear_right_motor_control_2.value(0)
+        self.rear_right_motor_control_2.value(1)
         self.rear_left_motor_control_1.value(1)
         self.rear_left_motor_control_2.value(0)
-        
+
     def set_speed(self, new_speed):
         self.speed = new_speed
-        
+
     def cleanUp(self):
-        print('Cleaning up pins')
+        print("Cleaning up pins")
         self.front_right_motor_enable_pin.deinit()
         self.front_left_motor_enable_pin.deinit()
         self.rear_right_motor_enable_pin.deinit()
