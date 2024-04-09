029965656f225a7977bd0805ecf035d4608eb1d5
Frank Siyli <siylif@eduroam-0372.desy.de>
2024-04-09 09:37:03 +0200

    seperate front sensor

--- esp32_and_esp8266_projects/mecanum_robot_car_iphone/02_ultasonic_sensors/files_on_esp32/ultrasonic_sensors.py
+++ esp32_and_esp8266_projects/mecanum_robot_car_iphone/02_ultasonic_sensors/files_on_esp32/ultrasonic_sensors.py
@@ -2,18 +2,34 @@ from machine import Pin
 from machine import time_pulse_us
 import time
 
-TRIG_PIN_NUM = 15
-ECHO_PIN_NUM = 13
+TRIG_PIN_FRONT = 15
+ECHO_PIN_FRONT = 13
 
-trig_pin = Pin(TRIG_PIN_NUM, Pin.OUT)
-echo_pin = Pin(ECHO_PIN_NUM, Pin.IN)
+TRIG_PIN_REAR = 0
+ECHO_PIN_REAR = 2
 
-def get_distance_cm():
-    trig_pin.value(0)
+trig_pin_front = Pin(TRIG_PIN_FRONT, Pin.OUT)
+echo_pin_front = Pin(ECHO_PIN_FRONT, Pin.IN)
+
+trig_pin_rear = Pin(TRIG_PIN_REAR, Pin.OUT)
+echo_pin_rear = Pin(ECHO_PIN_REAR, Pin.IN)
+
+def get_distance_front():
+    trig_pin_front.value(0)
     time.sleep_us(5)
-    trig_pin.value(1)
+    trig_pin_front.value(1)
     time.sleep_us(10)
-    trig_pin.value(0)
-    duration = time_pulse_us(echo_pin, 1)
-    distance_cm = duration / 58
-    return int(distance_cm)
+    trig_pin_front.value(0)
+    duration = time_pulse_us(echo_pin_front, 1)
+    distance_front = duration / 58
+    return int(distance_front)
+
+def get_distance_rear():
+    trig_pin_rear.value(0)
+    time.sleep_us(5)
+    trig_pin_rear.value(1)
+    time.sleep_us(10)
+    trig_pin_rear.value(0)
+    duration = time_pulse_us(echo_pin_rear, 1)
+    distance_rear = duration / 58
+    return int(distance_rear)
