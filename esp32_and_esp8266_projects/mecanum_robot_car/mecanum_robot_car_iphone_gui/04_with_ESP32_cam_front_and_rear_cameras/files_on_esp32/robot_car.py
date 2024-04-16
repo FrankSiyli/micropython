c8d28110879035e3adba9e134d6b730cffe02afe
Frank Siyli <siylif@eduroam-0372.desy.de>
2024-04-16 09:31:45 +0200

    front and rear camera displayed on the web server

---
+++ esp32_and_esp8266_projects/mecanum_robot_car/mecanum_robot_car_iphone_gui/04_with_ESP32_cam_front_and_rear_cameras/files_on_esp32/robot_car.py
@@ -0,0 +1,144 @@
+from machine import Pin, PWM
+
+
+
+class RobotCar:
+    def __init__(
+        self,
+        front_enable_pins,
+        rear_enable_pins,
+        front_motor_pins,
+        rear_motor_pins,
+        speed,
+    ):
+        self.front_right_motor_enable_pin = PWM(Pin(front_enable_pins[0]), freq=2000)
+        self.front_left_motor_enable_pin = PWM(Pin(front_enable_pins[1]), freq=2000)
+        self.rear_right_motor_enable_pin = PWM(Pin(rear_enable_pins[0]), freq=2000)
+        self.rear_left_motor_enable_pin = PWM(Pin(rear_enable_pins[1]), freq=2000)
+
+        self.front_right_motor_control_1 = Pin(front_motor_pins[0], Pin.OUT)
+        self.front_right_motor_control_2 = Pin(front_motor_pins[1], Pin.OUT)
+        self.front_left_motor_control_1 = Pin(front_motor_pins[2], Pin.OUT)
+        self.front_left_motor_control_2 = Pin(front_motor_pins[3], Pin.OUT)
+
+        self.rear_right_motor_control_1 = Pin(rear_motor_pins[0], Pin.OUT)
+        self.rear_right_motor_control_2 = Pin(rear_motor_pins[1], Pin.OUT)
+        self.rear_left_motor_control_1 = Pin(rear_motor_pins[2], Pin.OUT)
+        self.rear_left_motor_control_2 = Pin(rear_motor_pins[3], Pin.OUT)
+
+        self.speed = speed
+
+    def stop(self):
+        self.front_right_motor_control_1.value(0)
+        self.front_right_motor_control_2.value(0)
+        self.front_left_motor_control_1.value(0)
+        self.front_left_motor_control_2.value(0)
+        self.rear_right_motor_control_1.value(0)
+        self.rear_right_motor_control_2.value(0)
+        self.rear_left_motor_control_1.value(0)
+        self.rear_left_motor_control_2.value(0)
+
+        self.front_right_motor_enable_pin.duty_u16(0)
+        self.front_left_motor_enable_pin.duty_u16(0)
+        self.rear_right_motor_enable_pin.duty_u16(0)
+        self.rear_left_motor_enable_pin.duty_u16(0)
+
+    def forward(self):
+        self.front_right_motor_enable_pin.duty_u16(self.speed)
+        self.front_left_motor_enable_pin.duty_u16(self.speed)
+        self.rear_right_motor_enable_pin.duty_u16(self.speed)
+        self.rear_left_motor_enable_pin.duty_u16(self.speed)
+
+        self.front_right_motor_control_1.value(1)
+        self.front_right_motor_control_2.value(0)
+        self.front_left_motor_control_1.value(1)
+        self.front_left_motor_control_2.value(0)
+        self.rear_right_motor_control_1.value(1)
+        self.rear_right_motor_control_2.value(0)
+        self.rear_left_motor_control_1.value(1)
+        self.rear_left_motor_control_2.value(0)
+
+    def reverse(self):
+        self.front_right_motor_enable_pin.duty_u16(self.speed)
+        self.front_left_motor_enable_pin.duty_u16(self.speed)
+        self.rear_right_motor_enable_pin.duty_u16(self.speed)
+        self.rear_left_motor_enable_pin.duty_u16(self.speed)
+
+        self.front_right_motor_control_1.value(0)
+        self.front_right_motor_control_2.value(1)
+        self.front_left_motor_control_1.value(0)
+        self.front_left_motor_control_2.value(1)
+        self.rear_right_motor_control_1.value(0)
+        self.rear_right_motor_control_2.value(1)
+        self.rear_left_motor_control_1.value(0)
+        self.rear_left_motor_control_2.value(1)
+
+    def left(self):
+        self.front_right_motor_enable_pin.duty_u16(self.speed)
+        self.front_left_motor_enable_pin.duty_u16(self.speed)
+        self.rear_right_motor_enable_pin.duty_u16(self.speed)
+        self.rear_left_motor_enable_pin.duty_u16(self.speed)
+
+        self.front_right_motor_control_1.value(0)
+        self.front_right_motor_control_2.value(1)
+        self.front_left_motor_control_1.value(1)
+        self.front_left_motor_control_2.value(0)
+        self.rear_right_motor_control_1.value(1)
+        self.rear_right_motor_control_2.value(0)
+        self.rear_left_motor_control_1.value(0)
+        self.rear_left_motor_control_2.value(1)
+
+    def right(self):
+        self.front_right_motor_enable_pin.duty_u16(self.speed)
+        self.front_left_motor_enable_pin.duty_u16(self.speed)
+        self.rear_right_motor_enable_pin.duty_u16(self.speed)
+        self.rear_left_motor_enable_pin.duty_u16(self.speed)
+
+        self.front_right_motor_control_1.value(1)
+        self.front_right_motor_control_2.value(0)
+        self.front_left_motor_control_1.value(0)
+        self.front_left_motor_control_2.value(1)
+        self.rear_right_motor_control_1.value(0)
+        self.rear_right_motor_control_2.value(1)
+        self.rear_left_motor_control_1.value(1)
+        self.rear_left_motor_control_2.value(0)
+
+    def spinLeft(self):
+        self.front_right_motor_enable_pin.duty_u16(self.speed)
+        self.front_left_motor_enable_pin.duty_u16(self.speed)
+        self.rear_right_motor_enable_pin.duty_u16(self.speed)
+        self.rear_left_motor_enable_pin.duty_u16(self.speed)
+
+        self.front_right_motor_control_1.value(1)
+        self.front_right_motor_control_2.value(0)
+        self.front_left_motor_control_1.value(0)
+        self.front_left_motor_control_2.value(1)
+        self.rear_right_motor_control_1.value(1)
+        self.rear_right_motor_control_2.value(0)
+        self.rear_left_motor_control_1.value(0)
+        self.rear_left_motor_control_2.value(1)
+
+    def spinRight(self):
+        self.front_right_motor_enable_pin.duty_u16(self.speed)
+        self.front_left_motor_enable_pin.duty_u16(self.speed)
+        self.rear_right_motor_enable_pin.duty_u16(self.speed)
+        self.rear_left_motor_enable_pin.duty_u16(self.speed)
+
+        self.front_right_motor_control_1.value(0)
+        self.front_right_motor_control_2.value(1)
+        self.front_left_motor_control_1.value(1)
+        self.front_left_motor_control_2.value(0)
+        self.rear_right_motor_control_1.value(0)
+        self.rear_right_motor_control_2.value(1)
+        self.rear_left_motor_control_1.value(1)
+        self.rear_left_motor_control_2.value(0)
+
+    def set_speed(self, new_speed):
+        self.speed = new_speed
+
+    def cleanUp(self):
+        print("Cleaning up pins")
+        self.front_right_motor_enable_pin.deinit()
+        self.front_left_motor_enable_pin.deinit()
+        self.rear_right_motor_enable_pin.deinit()
+        self.rear_left_motor_enable_pin.deinit()
---
+++ esp32_and_esp8266_projects/mecanum_robot_car/mecanum_robot_car_iphone_gui/04_with_ESP32_cam_front_and_rear_cameras/files_on_esp32/static/js/custom.js
@@ -0,0 +1,95 @@
+var targetUrl = `ws://${location.host}/ws`;
+var websocket;
+
+function initializeSocket() {
+  console.log("Opening WebSocket connection to ESP32 MicroPython Server...");
+  websocket = new WebSocket(targetUrl);
+  websocket.onopen = onOpen;
+  websocket.onclose = onClose;
+  websocket.onmessage = onMessage;
+}
+
+function onLoad() {
+  initializeSocket();
+  setDefaultSpeed();
+  setInterval(updateDistanceValue, 500);
+}
+
+function onOpen(event) {
+  console.log("Starting connection to WebSocket server..");
+}
+
+function onClose(event) {
+  console.log("Closing connection to server..");
+  setTimeout(initializeSocket, 2000);
+}
+
+function onMessage(event) {
+  var data = JSON.parse(event.data);
+  if (data.distance_front && data.distance_rear !== undefined) {
+    document.getElementById(
+      "distance-front-value"
+    ).innerText = `Front distance: ${data.distance_front} cm`;
+    document.getElementById(
+      "distance-rear-value"
+    ).innerText = `Rear distance: ${data.distance_rear} cm`;
+  }
+}
+
+function updateDistanceValue() {
+  // Send a message to request the distance value from the server
+  websocket.send("request_distance");
+}
+
+function sendMessage(message) {
+  websocket.send(message);
+}
+
+// Speed Settings Handler
+var speedSettings = document.querySelectorAll(
+  'input[type=radio][name="speed-settings"]'
+);
+speedSettings.forEach((radio) =>
+  radio.addEventListener("change", () => {
+    var speedSettings = radio.value;
+    console.log("Speed Settings :: " + speedSettings);
+    sendMessage(speedSettings);
+  })
+);
+
+function setDefaultSpeed() {
+  console.log("Setting default speed to normal..");
+  let normalOption = document.getElementById("option-2");
+  normalOption.checked = true;
+}
+
+// O-Pad/ D-Pad Controller and Javascript Code
+document.body.addEventListener("click", function (e) {
+  if (e.target && e.target.nodeName == "A") {
+    e.preventDefault();
+  }
+});
+
+function touchStartHandler(event) {
+  var direction = event.target.dataset.direction;
+  console.log("Touch Start :: " + direction);
+  sendMessage("car-" + direction);
+}
+
+function touchEndHandler(event) {
+  const stop_command = "car-stop";
+  var direction = event.target.dataset.direction;
+  console.log("Touch End :: " + direction);
+  sendMessage(stop_command);
+}
+
+document.querySelectorAll(".control").forEach((item) => {
+  item.addEventListener("touchstart", touchStartHandler);
+});
+
+document.querySelectorAll(".control").forEach((item) => {
+  item.addEventListener("touchend", touchEndHandler);
+});
+
+// Call onLoad function when the window loads
+window.addEventListener("load", onLoad);
